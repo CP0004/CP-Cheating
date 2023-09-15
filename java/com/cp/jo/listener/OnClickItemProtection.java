ceb003ae1c6c1263e822051c292b2098d2772dcf
Mohamad <90915776+CP0004@users.noreply.github.com>
2023-09-15 18:28:54 +0300

    Delete java/com/cp/jo directory

--- java/com/cp/jo/listener/OnClickItemProtection.java
+++
@@ -1,9 +0,0 @@
-package com.cp.jo.listener;
-
-import com.cp.jo.model.ModelProtection;
-
-import java.util.List;
-
-public interface OnClickItemProtection {
-    void onClickItemProtection(List<ModelProtection> modelProtection, int position);
-}
